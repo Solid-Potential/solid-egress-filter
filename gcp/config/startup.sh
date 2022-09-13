1db3274e9947660f30d6a481128d55ab153befcc
Marcin Natanek <marcin.natanek@solid-potential.dev>
2022-09-13 20:37:23 +0200

    startup script draft

--- gcp/config/startup.sh
+++ gcp/config/startup.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-echo "test"
+
+set -x # Debug
+set -e # Strict exit codes
+
+echo "Applying system patches"
+sudo apt update
+sudo apt upgrade
+
+echo "Installing Ops Agent"
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+echo "Installing MITM Proxy"
+wget https://snapshots.mitmproxy.org/8.1.1/mitmproxy-8.1.1-linux.tar.gz
+# TODO untar and install
+
+echo "Apply MITM configuration"
+# TODO
+
+echo "Configure Ops Agent log streams"
+# TODO
+
+echo "Healthcheck API"
+# TODO
