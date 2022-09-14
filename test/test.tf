2753e6b40e683f7605249c61a21ab37755d789db
Marcin Natanek <marcin.natanek@solid-potential.dev>
2022-09-14 14:13:54 +0200

    test: applied to gcp

--- test/test.tf
+++ test/test.tf
@@ -1,13 +1,15 @@
 provider "google" {
+  project = "solid-egress-filter"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
 }
 
 terraform {
   backend "gcs" {
-    bucket = "todo"
+    bucket = "solid-terraform-state"
   }
 }
 
 module "egress_filter" {
-    source = "../gcp"
-    project = "todo"
+  source = "../gcp"
 }
