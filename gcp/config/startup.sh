22e5067b6ed57d465f1cc1fc31909b61d63eafb8
Krzysztof Baum <krzysztof.baum@solid-potential.dev>
2022-09-16 17:14:04 +0200

    configure packer

--- gcp/config/startup.sh
+++ gcp/config/startup.sh
@@ -5,15 +5,21 @@ set -e # Strict exit codes
 
 echo "Applying system patches"
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
+
+whoami
 
 echo "Installing Ops Agent"
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install -y
+
+echo "Installing wget"
+sudo apt install wget -y
 
 echo "Installing MITM Proxy"
 wget https://snapshots.mitmproxy.org/8.1.1/mitmproxy-8.1.1-linux.tar.gz
-# TODO untar and install
+tar vczf mitmproxy-8.1.1-linux.tar.gz
+rm mitmproxy-8.1.1-linux.tar.gz
 
 echo "Apply MITM configuration"
 # TODO
