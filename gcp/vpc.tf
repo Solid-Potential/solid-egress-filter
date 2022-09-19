5f2aa67d7f530ebcd4563ea576c7efbf5110aa90
Marcin Natanek <marcin.natanek@solid-potential.dev>
2022-09-19 16:29:41 +0200

    #6 - firewall - default deny and filter allow

--- gcp/vpc.tf
+++ gcp/vpc.tf
@@ -1,7 +1,7 @@
 resource "google_compute_route" "default_internet_gateway" {
   project          = var.project
   description      = "Default egress route for Solid Egress Filter instances"
-  name             = "${local.prefix}-default-internet-gateway"
+  name             = "${local.prefix}default-internet-gateway"
   dest_range       = "0.0.0.0/0"
   tags             = [var.filter_network_tag]
   network          = var.vpc_name
@@ -13,7 +13,7 @@ resource "google_compute_route" "default_internet_gateway" {
 # resource "google_compute_route" "forward_to_egress_filter" {
 #   project      = var.project
 #   description  = "Forward all internet egress to Egress Filter"
-#   name         = "${local.prefix}-forward-to-egress-filter"
+#   name         = "${local.prefix}forward-to-egress-filter"
 #   dest_range   = "0.0.0.0/0"
 #   network      = var.vpc_name
 #   priority     = 999
@@ -22,7 +22,7 @@ resource "google_compute_route" "default_internet_gateway" {
 
 resource "google_compute_route" "restricted_google_apis" {
   project = var.project
-  name    = "${local.prefix}-restricted-google-apis"
+  name    = "${local.prefix}restricted-google-apis"
   # Restricted connectivitiy to google api
   # https://cloud.google.com/vpc-service-controls/docs/set-up-private-connectivity
   dest_range       = "199.36.153.4/30"
@@ -33,7 +33,7 @@ resource "google_compute_route" "restricted_google_apis" {
 
 resource "google_compute_route" "private_google_apis" {
   project = var.project
-  name    = "${local.prefix}-private-google-apis"
+  name    = "${local.prefix}private-google-apis"
   # Private connectivitiy to google api
   # https://cloud.google.com/vpc-service-controls/docs/set-up-private-connectivity
   dest_range       = "199.36.153.8/30"
@@ -44,7 +44,7 @@ resource "google_compute_route" "private_google_apis" {
 
 resource "google_compute_route" "restricted_google_apis_filter" {
   project = var.project
-  name    = "${local.prefix}-restricted-google-apis-filter"
+  name    = "${local.prefix}restricted-google-apis-filter"
   # Restricted connectivitiy to google api
   # https://cloud.google.com/vpc-service-controls/docs/set-up-private-connectivity
   tags             = [var.filter_network_tag]
@@ -56,7 +56,7 @@ resource "google_compute_route" "restricted_google_apis_filter" {
 
 resource "google_compute_route" "private_google_apis_filter" {
   project = var.project
-  name    = "${local.prefix}-private-google-apis-filter"
+  name    = "${local.prefix}private-google-apis-filter"
   # Private connectivitiy to google api
   # https://cloud.google.com/vpc-service-controls/docs/set-up-private-connectivity
   tags             = [var.filter_network_tag]
