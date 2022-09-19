d1f1b34ba9c3fe3f036cba9583e6357f9f600b2b
Krzysztof Baum <krzysztof.baum@solid-potential.dev>
2022-09-19 13:33:07 +0200

    fix startup script, use ubuntu as base image

--- gcp/config/startup.sh
+++ gcp/config/startup.sh
@@ -4,28 +4,32 @@ set -x # Debug
 set -e # Strict exit codes
 
 echo "Applying system patches"
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update;
 
-whoami
+sudo apt-get upgrade -y;
 
-echo "Installing Ops Agent"
-curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install -y
 
-echo "Installing wget"
-sudo apt install wget -y
+echo "Installing Ops Agent";
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh;
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install ;
+rm add-google-cloud-ops-agent-repo.sh;
 
-echo "Installing MITM Proxy"
-wget https://snapshots.mitmproxy.org/8.1.1/mitmproxy-8.1.1-linux.tar.gz
-tar -zxvf mitmproxy-8.1.1-linux.tar.gz
-rm mitmproxy-8.1.1-linux.tar.gz
+echo "Installing wget";
+sudo apt-get install wget -y;
 
-echo "Apply MITM configuration"
+echo "Installing MITM Proxy";
+wget https://snapshots.mitmproxy.org/8.1.1/mitmproxy-8.1.1-linux.tar.gz;
+tar -zxvf mitmproxy-8.1.1-linux.tar.gz;
+rm mitmproxy-8.1.1-linux.tar.gz;
+
+echo "Apply MITM configuration";
 # TODO
 
-echo "Configure Ops Agent log streams"
+echo "Configure Ops Agent log streams";
 # TODO
 
-echo "Healthcheck API"
+echo "Healthcheck API";
 # TODO
+
+
+sudo reboot;
