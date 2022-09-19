dabeec23ae3200427b154173d4a40d2a31321e90
Emil Dussebaev <e.dussebaev@reply.com>
2022-09-19 16:28:48 +0200

    Add Internal Load Balancer resources

---
+++ gcp/ilb.tf
@@ -0,0 +1,58 @@
+# # VPC
+# resource "google_compute_network" "ilb_network" {
+#   name = "default"
+# }
+
+# # backed subnet
+# resource "google_compute_subnetwork" "ilb_subnet" {
+#   name   = "default"
+#   region = var.region
+#   ip_cidr_range = "10.132.0.0/20"
+
+#   network = google_compute_network.ilb_network.id
+# }
+
+# forwarding rule
+resource "google_compute_forwarding_rule" "egress_filter_forwarding_rule" {
+  name            = "egress-filter-forwarding-rule"
+  backend_service = google_compute_region_backend_service.egress_filter_backend.id
+
+  region = var.region
+
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "INTERNAL"
+  all_ports             = true
+  allow_global_access   = true
+
+  network    = var.vpc_name
+  subnetwork = var.subnet_name
+}
+
+# backend service
+resource "google_compute_region_backend_service" "egress_filter_backend" {
+  name   = "egress-filter-ilb-backend"
+  region = var.region
+
+  protocol              = "TCP"
+  load_balancing_scheme = "INTERNAL"
+  health_checks         = [google_compute_region_health_check.egress_filter.id]
+
+  backend {
+    group          = google_compute_region_instance_group_manager.egress_filter.instance_group
+    balancing_mode = "CONNECTION"
+  }
+}
+
+resource "google_compute_region_health_check" "egress_filter" {
+  name   = "egress-filter-health-check"
+  region = var.region
+
+  timeout_sec         = 5
+  check_interval_sec  = 5
+  healthy_threshold   = 5
+  unhealthy_threshold = 10
+
+  tcp_health_check {
+	port = 8080
+  }
+}
